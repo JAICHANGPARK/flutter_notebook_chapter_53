47bc17c6fec466801d07781bfe1ccdc0e6605d0e
JAICHANGPARK <aristojeff@gmail.com>
2026-07-23 18:24:22 +0900

    Update fitness_app.dart

--- lib/ep2651_fitness_app/fitness_app.dart
+++ lib/ep2651_fitness_app/fitness_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_53/ep2651_fitness_app/ui/fitness_home_page.dart';
 
 
 class FitnessApp extends StatelessWidget {
@@ -7,7 +8,7 @@ class FitnessApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
+      home: FitnessHomePage(),
     );
   }
 }
