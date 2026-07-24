e5d78d4d8f996bdc097a03388b55d13d88b83102
JAICHANGPARK <aristojeff@gmail.com>
2026-07-24 17:50:24 +0900

    Update fitness_home_page.dart

--- lib/ep2651_fitness_app/ui/fitness_home_page.dart
+++ lib/ep2651_fitness_app/ui/fitness_home_page.dart
@@ -173,9 +173,13 @@ class _FitnessHomePageState extends State<FitnessHomePage> {
                         children: List.generate(
                           5,
                           (idx) => Container(
-                            decoration: ShapeDecoration(shape: RoundedSuperellipseBorder(side: BorderSide(
-                              color: Colors.white24
-                            )),),
+                            decoration: ShapeDecoration(
+                              shape: RoundedSuperellipseBorder(
+                                borderRadius: .circular(16),
+                                side: BorderSide(color: Colors.white24),
+                              ),
+                              
+                            ),
                             margin: .only(bottom: 16),
                             height: 100,
                             child: Row(children: []),
