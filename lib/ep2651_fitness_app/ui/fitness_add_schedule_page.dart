0037534344b860f2f6882e5836e2d38980f63b2f
JAICHANGPARK <aristojeff@gmail.com>
2026-07-27 18:16:46 +0900

    Update fitness_add_schedule_page.dart

--- lib/ep2651_fitness_app/ui/fitness_add_schedule_page.dart
+++ lib/ep2651_fitness_app/ui/fitness_add_schedule_page.dart
@@ -1,1 +1,15 @@
 import 'package:flutter/material.dart';
+
+class FitnessAddSchedulePage extends StatefulWidget {
+  const FitnessAddSchedulePage({super.key});
+
+  @override
+  State<FitnessAddSchedulePage> createState() => _FitnessAddSchedulePageState();
+}
+
+class _FitnessAddSchedulePageState extends State<FitnessAddSchedulePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
