6e1f76df907b9636c2c62ab64925eaccc2c19b2f
JAICHANGPARK <aristojeff@gmail.com>
2026-07-30 19:05:25 +0900

    Update education_main_page.dart

--- lib/ep2657_education_app/ui/education_main_page.dart
+++ lib/ep2657_education_app/ui/education_main_page.dart
@@ -11,22 +11,34 @@ class _EducationMainPageState extends State<EducationMainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        backgroundColor: Colors.grey[100],
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home_filled), label: "Home"),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.calendar_month),
-            label: "Calendar",
+      backgroundColor: Colors.white,
+      bottomNavigationBar: ClipRRect(
+        borderRadius: BorderRadius.only(topLeft: .circular(32)),
+        child: Container(
+          height: 110,
+          decoration: BoxDecoration(),
+          child: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+
+            backgroundColor: Colors.grey[100],
+            items: [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home_filled),
+                label: "Home",
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.calendar_month),
+                label: "Calendar",
+              ),
+              BottomNavigationBarItem(icon: Icon(Icons.phone), label: "Calls"),
+              BottomNavigationBarItem(icon: Icon(Icons.chat), label: "Chat"),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.person_2_outlined),
+                label: "Profile",
+              ),
+            ],
           ),
-          BottomNavigationBarItem(icon: Icon(Icons.phone), label: "Calls"),
-          BottomNavigationBarItem(icon: Icon(Icons.chat), label: "Chat"),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person_2_outlined),
-            label: "Profile",
-          ),
-        ],
+        ),
       ),
     );
   }
