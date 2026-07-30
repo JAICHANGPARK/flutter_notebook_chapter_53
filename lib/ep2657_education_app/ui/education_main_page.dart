9536306360b64eaf3bd9f0fb6b73073bcb055863
JAICHANGPARK <aristojeff@gmail.com>
2026-07-30 18:59:28 +0900

    Create education_main_page.dart

---
+++ lib/ep2657_education_app/ui/education_main_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class EducationMainPage extends StatefulWidget {
+  const EducationMainPage({super.key});
+
+  @override
+  State<EducationMainPage> createState() => _EducationMainPageState();
+}
+
+class _EducationMainPageState extends State<EducationMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
