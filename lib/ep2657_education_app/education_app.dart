9834e93cc00132f6b91b078dd7d849a18171bc5b
JAICHANGPARK <aristojeff@gmail.com>
2026-07-30 18:59:33 +0900

    Update education_app.dart

--- lib/ep2657_education_app/education_app.dart
+++ lib/ep2657_education_app/education_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_53/ep2657_education_app/ui/education_home_page.dart';
 
 
 class EducationApp extends StatelessWidget {
@@ -7,7 +8,7 @@ class EducationApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
+      home: EducationHomePage(),
     );
   }
 }
