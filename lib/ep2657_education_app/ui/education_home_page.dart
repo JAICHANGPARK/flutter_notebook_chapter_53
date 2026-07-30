ecaf631c514865eedd7ec1f30b230d0e8219eb10
JAICHANGPARK <aristojeff@gmail.com>
2026-07-30 18:58:07 +0900

    Update education_home_page.dart

---
+++ lib/ep2657_education_app/ui/education_home_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class EducationHomePage extends StatefulWidget {
+  const EducationHomePage({super.key});
+
+  @override
+  State<EducationHomePage> createState() => _EducationHomePageState();
+}
+
+class _EducationHomePageState extends State<EducationHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
