bf8479b8752d357c9b4c9ac63cfbb18115aefc34
JAICHANGPARK <aristojeff@gmail.com>
2026-08-01 08:50:09 +0900

    Update education_calendar_page.dart

--- lib/ep2657_education_app/ui/education_calendar_page.dart
+++ lib/ep2657_education_app/ui/education_calendar_page.dart
@@ -10,33 +10,35 @@ class EducationCalendarPage extends StatefulWidget {
 class _EducationCalendarPageState extends State<EducationCalendarPage> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: .start,
-      spacing: 20,
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(left: 16),
-          child: Column(
-            spacing: 16,
-            crossAxisAlignment: .start,
-            children: [
-              Text("Jun 2026",style: TextStyle(
-                fontWeight: .bold,
-                fontSize: 18,
-              ),),
-              SizedBox(height: 100, child: Placeholder()),
-            ],
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: .start,
+        spacing: 20,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 16),
+            child: Column(
+              spacing: 16,
+              crossAxisAlignment: .start,
+              children: [
+                Text("Jun 2026",style: TextStyle(
+                  fontWeight: .bold,
+                  fontSize: 18,
+                ),),
+                SizedBox(height: 100, child: Placeholder()),
+              ],
+            ),
           ),
-        ),
-        Container(
-          height: 200,
-          child: Placeholder(),
-        ),
-        Container(
-          height: 320,
-          child: Placeholder(),
-        )
-      ],
+          Container(
+            height: 200,
+            child: Placeholder(),
+          ),
+          Container(
+            height: 320,
+            child: Placeholder(),
+          )
+        ],
+      ),
     );
   }
 }
