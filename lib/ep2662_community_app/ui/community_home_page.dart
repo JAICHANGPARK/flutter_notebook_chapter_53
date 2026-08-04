b8160ed16543a5ebfa11276291e09a4273d6fe63
JAICHANGPARK <aristojeff@gmail.com>
2026-08-04 18:27:25 +0900

    Update community_home_page.dart

---
+++ lib/ep2662_community_app/ui/community_home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class CommunityHomePage extends StatefulWidget {
+  const CommunityHomePage({super.key});
+
+  @override
+  State<CommunityHomePage> createState() => _CommunityHomePageState();
+}
+
+class _CommunityHomePageState extends State<CommunityHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
