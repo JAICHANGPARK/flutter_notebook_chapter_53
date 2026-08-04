4635639813b44d3024820cd7ee69e0b6194e3ea4
JAICHANGPARK <aristojeff@gmail.com>
2026-08-04 18:27:24 +0900

    Update community_main_page.dart

---
+++ lib/ep2662_community_app/ui/community_main_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class CommunityMainPage extends StatefulWidget {
+  const CommunityMainPage({super.key});
+
+  @override
+  State<CommunityMainPage> createState() => _CommunityMainPageState();
+}
+
+class _CommunityMainPageState extends State<CommunityMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
