1d0adf3a34d5820ca3bced01dd8af7b74b1b2a80
Dreamwalker <aristojeff@gmail.com>
2026-08-11 23:18:12 +0800

    Update community_main_page.dart

--- lib/ep2662_community_app/ui/community_main_page.dart
+++ lib/ep2662_community_app/ui/community_main_page.dart
@@ -33,7 +33,7 @@ class _CommunityMainPageState extends State<CommunityMainPage> {
             icon: Icon(Icons.access_time_outlined),
             label: "Player",
           ),
-          BottomNavigationBarItem(icon: Icon(Icons.event), label: "Events"),
+          BottomNavigationBarItem(icon: Icon(Icons.event), label: "Events",),
           BottomNavigationBarItem(
             icon: Icon(Icons.card_giftcard),
             label: "Donate",
