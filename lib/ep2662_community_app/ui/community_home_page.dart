48c298bb2e154507aecae998a8baa0569b6b0119
JAICHANGPARK <aristojeff@gmail.com>
2026-08-19 20:03:32 +0900

    Update community_home_page.dart

--- lib/ep2662_community_app/ui/community_home_page.dart
+++ lib/ep2662_community_app/ui/community_home_page.dart
@@ -517,7 +517,7 @@ class _CommunityHomePageState extends State<CommunityHomePage> {
                                         color: Colors.grey[200]!,
                                       ),
                                     ),
-                                  ),
+                                  ),  padding: .symmetric(horizontal: 16),
                                   child: Center(
                                     child: Text(
                                       "Quran Tafsir",
