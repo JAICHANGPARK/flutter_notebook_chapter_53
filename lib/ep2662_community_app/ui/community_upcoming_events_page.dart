c29a0c5e2da09f16f5458ff9248a63238902a63c
JAICHANGPARK <aristojeff@gmail.com>
2026-08-20 18:30:29 +0900

    Update community_upcoming_events_page.dart

--- lib/ep2662_community_app/ui/community_upcoming_events_page.dart
+++ lib/ep2662_community_app/ui/community_upcoming_events_page.dart
@@ -1,18 +1,17 @@
 import 'package:flutter/material.dart';
 
-
 class CommunityUpcomingEventsPage extends StatefulWidget {
   const CommunityUpcomingEventsPage({super.key});
 
   @override
-  State<CommunityUpcomingEventsPage> createState() => _CommunityUpcomingEventsPageState();
+  State<CommunityUpcomingEventsPage> createState() =>
+      _CommunityUpcomingEventsPageState();
 }
 
-class _CommunityUpcomingEventsPageState extends State<CommunityUpcomingEventsPage> {
+class _CommunityUpcomingEventsPageState
+    extends State<CommunityUpcomingEventsPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-
-    );
+    return Scaffold(backgroundColor: Color.fromRGBO(246, 248, 250, 1));
   }
 }
