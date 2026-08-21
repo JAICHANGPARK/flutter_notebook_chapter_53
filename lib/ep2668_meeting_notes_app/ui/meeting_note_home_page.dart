714660e834fbe52b93a70b3d50a82fcc75b53a09
JAICHANGPARK <aristojeff@gmail.com>
2026-08-21 18:37:29 +0900

    Create meeting_note_home_page.dart

---
+++ lib/ep2668_meeting_notes_app/ui/meeting_note_home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class MeetingNoteHomePage extends StatefulWidget {
+  const MeetingNoteHomePage({super.key});
+
+  @override
+  State<MeetingNoteHomePage> createState() => _MeetingNoteHomePageState();
+}
+
+class _MeetingNoteHomePageState extends State<MeetingNoteHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
