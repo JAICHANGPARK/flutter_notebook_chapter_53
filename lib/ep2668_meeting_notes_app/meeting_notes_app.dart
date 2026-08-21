501ef7a9916d1513777f7f37dc912b108608f2c6
JAICHANGPARK <aristojeff@gmail.com>
2026-08-21 18:38:36 +0900

    Update meeting_notes_app.dart

---
+++ lib/ep2668_meeting_notes_app/meeting_notes_app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class MeetingNotesApp extends StatelessWidget {
+  const MeetingNotesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
