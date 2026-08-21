98d1eba14d7722320f46f328e1a93b385dc0a532
JAICHANGPARK <aristojeff@gmail.com>
2026-08-21 18:50:18 +0900

    Update meeting_notes_app.dart

--- lib/ep2668_meeting_notes_app/meeting_notes_app.dart
+++ lib/ep2668_meeting_notes_app/meeting_notes_app.dart
@@ -1,10 +1,14 @@
 import 'package:material_ui/material_ui.dart';
 
+import 'ui/meeting_note_home_page.dart';
+
 class MeetingNotesApp extends StatelessWidget {
   const MeetingNotesApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: MeetingNoteHomePage(),
+    );
   }
 }
