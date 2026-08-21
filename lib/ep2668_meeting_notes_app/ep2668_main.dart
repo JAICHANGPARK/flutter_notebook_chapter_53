26cecb62065601505c270ff3d59424f95641b91f
JAICHANGPARK <aristojeff@gmail.com>
2026-08-21 18:40:42 +0900

    Update ep2668_main.dart

--- lib/ep2668_meeting_notes_app/ep2668_main.dart
+++ lib/ep2668_meeting_notes_app/ep2668_main.dart
@@ -1,5 +1,8 @@
 
 
-void main() {
+import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_53/ep2668_meeting_notes_app/meeting_notes_app.dart';
 
+void main() {
+    runApp(MeetingNotesApp());
 }
