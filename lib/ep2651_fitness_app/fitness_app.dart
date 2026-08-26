baad2f3a6f668cc251edede42e9dfab2bd54d9ca
Dreamwalker <aristojeff@gmail.com>
2026-08-26 18:09:48 +0800

    fitness_app.dart 업데이트

--- lib/ep2651_fitness_app/fitness_app.dart
+++ lib/ep2651_fitness_app/fitness_app.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_notebook_chapter_53/ep2651_fitness_app/ui/fitness_home_page.dart';
 
-
 class FitnessApp extends StatelessWidget {
   const FitnessApp({super.key});
 
