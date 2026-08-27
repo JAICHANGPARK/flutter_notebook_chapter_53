bc76a70e575332ba9a49244892679f3028b80336
JAICHANGPARK <aristojeff@gmail.com>
2026-08-27 18:34:44 +0900

    Update meeting_note_meeting_page.dart

--- lib/ep2668_meeting_notes_app/ui/meeting_note_meeting_page.dart
+++ lib/ep2668_meeting_notes_app/ui/meeting_note_meeting_page.dart
@@ -1,1 +1,19 @@
 import 'package:material_ui/material_ui.dart';
+
+class MeetingNoteMeetingPage extends StatefulWidget {
+  const MeetingNoteMeetingPage({super.key});
+
+  @override
+  State<MeetingNoteMeetingPage> createState() => _MeetingNoteMeetingPageState();
+}
+
+class _MeetingNoteMeetingPageState extends State<MeetingNoteMeetingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+
+      ],
+    );
+  }
+}
