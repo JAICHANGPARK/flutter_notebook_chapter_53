be4fb0e0400d15616b544d538e47932d95ec1214
JAICHANGPARK <aristojeff@gmail.com>
2026-09-08 18:29:06 +0900

    Create mental_health_home_page.dart

---
+++ lib/ep2674_mental_health_app/ui/mental_health_home_page.dart
@@ -0,0 +1,11 @@
+import 'package:material_ui/material_ui.dart';
+
+
+class MentalHealthHomePage extends StatelessWidget {
+  const MentalHealthHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
