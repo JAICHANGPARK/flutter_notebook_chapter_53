2a7a11ff7b348d2c6a664545ca6f0384e6acbca2
JAICHANGPARK <aristojeff@gmail.com>
2026-09-08 18:15:20 +0900

    Create mental_health_app.dart

---
+++ lib/ep2674_mental_health_app/mental_health_app.dart
@@ -0,0 +1,11 @@
+import 'package:material_ui/material_ui.dart';
+
+
+class MentalHealthApp extends StatelessWidget {
+  const MentalHealthApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
