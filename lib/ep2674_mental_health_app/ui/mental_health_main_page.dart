b1c4db9e98e9623795c18775317bfc7ebbeae329
JAICHANGPARK <aristojeff@gmail.com>
2026-09-08 18:15:59 +0900

    Create mental_health_main_page.dart

---
+++ lib/ep2674_mental_health_app/ui/mental_health_main_page.dart
@@ -0,0 +1,16 @@
+import 'package:material_ui/material_ui.dart';
+
+
+class MentalHealthMainPage extends StatefulWidget {
+  const MentalHealthMainPage({super.key});
+
+  @override
+  State<MentalHealthMainPage> createState() => _MentalHealthMainPageState();
+}
+
+class _MentalHealthMainPageState extends State<MentalHealthMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
