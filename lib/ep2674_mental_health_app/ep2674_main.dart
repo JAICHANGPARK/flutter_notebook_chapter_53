c310f3138d500b86ef58cf25d5bed696ea64f7bb
JAICHANGPARK <aristojeff@gmail.com>
2026-09-08 18:17:10 +0900

    Update ep2674_main.dart

--- lib/ep2674_mental_health_app/ep2674_main.dart
+++ lib/ep2674_mental_health_app/ep2674_main.dart
@@ -1,6 +1,7 @@
+import 'package:flutter_notebook_chapter_53/ep2674_mental_health_app/mental_health_app.dart';
 import 'package:material_ui/material_ui.dart';
 
 
 void main() {
-
+  runApp(MentalHealthApp());
 }
