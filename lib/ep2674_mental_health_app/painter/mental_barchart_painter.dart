0aba8cb5d5a5ccd82cb19de6d9d515d648255a5c
JAICHANGPARK <aristojeff@gmail.com>
2026-09-11 17:34:00 +0900

    Update mental_barchart_painter.dart

--- lib/ep2674_mental_health_app/painter/mental_barchart_painter.dart
+++ lib/ep2674_mental_health_app/painter/mental_barchart_painter.dart
@@ -3,21 +3,24 @@ import 'package:flutter/material.dart';
 class MentalBarchartPainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
-    final barHeight = [0.4, 0.45, 0.35];
-    final barCount = barHeight.length;
+    final barHeights = [
+      0.35, 0.48, 0.40, 0.65, 0.85, 1.0, 0.80, 0.55, 0.28, 0.42, 0.40, 0.28, 0.22, 0.18, 0.12
+    ];
+    final barCount = barHeights.length;
     final spacing = 4.0;
     final barWidth = (size.width - (spacing * (barCount - 1))) / barCount;
 
     final paint = Paint()..style = .fill;
     for (int i = 0; i < barCount; i++) {
       final x = i * (barWidth + spacing);
-      final h = size.height * barHeight[i];
+      final h = size.height * barHeights[i];
+
       final rect = RRect.fromRectAndRadius(
-        Rect.fromLTWH(x, size.height, barWidth, h),
-        Radius.circular(2),
+        Rect.fromLTWH(x, size.height - h, barWidth, h),
+        Radius.circular(barWidth / 2),
       );
 
-      final colorOpacity = (barHeight[i] * .7 + .3).clamp(.1, 1).toDouble();
+      final colorOpacity = (barHeights[i] * .7 + .3).clamp(.1, 1).toDouble();
       paint.color = Colors.orangeAccent.withValues(alpha: colorOpacity);
 
       canvas.drawRRect(rect, paint);
