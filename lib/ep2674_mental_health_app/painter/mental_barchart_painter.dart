5da06745d673a494f6249a371e4e590ce3a2043f
JAICHANGPARK <aristojeff@gmail.com>
2026-09-11 17:24:24 +0900

    Update mental_barchart_painter.dart

--- lib/ep2674_mental_health_app/painter/mental_barchart_painter.dart
+++ lib/ep2674_mental_health_app/painter/mental_barchart_painter.dart
@@ -3,7 +3,12 @@ import 'package:flutter/material.dart';
 class MentalBarchartPainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
-    // TODO: implement paint
+    final barHeight = [0.4, 0.45, 0.35];
+    final barCount = barHeight.length;
+
+    
+
+
   }
 
   @override
