c011687d5ed9527911cde2afa1cb4977be7f9552
JAICHANGPARK <aristojeff@gmail.com>
2026-09-12 09:08:59 +0900

    Update mental_journal_grid_painter.dart

--- lib/ep2674_mental_health_app/painter/mental_journal_grid_painter.dart
+++ lib/ep2674_mental_health_app/painter/mental_journal_grid_painter.dart
@@ -9,7 +9,9 @@ class MentalJournalGridPainter extends CustomPainter{
     const cols = 8;
     const spacing = 4.0;
 
-    
+    final cellSize = (size.width - (spacing * (cols - 1))) / cols;
+    final paint = Paint()..style = .fill;
+
   }
 
   @override
