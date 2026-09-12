d86877de0dafe9221aabe299855c4034014671df
JAICHANGPARK <aristojeff@gmail.com>
2026-09-12 09:08:30 +0900

    Update mental_journal_grid_painter.dart

--- lib/ep2674_mental_health_app/painter/mental_journal_grid_painter.dart
+++ lib/ep2674_mental_health_app/painter/mental_journal_grid_painter.dart
@@ -3,12 +3,18 @@ import 'package:flutter/material.dart';
 class MentalJournalGridPainter extends CustomPainter{
   @override
   void paint(Canvas canvas, Size size) {
+    const gridData = [];
 
+    const rows = 4;
+    const cols = 8;
+    const spacing = 4.0;
+
+    
   }
 
   @override
   bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    
+
     return false;
   }
 }
