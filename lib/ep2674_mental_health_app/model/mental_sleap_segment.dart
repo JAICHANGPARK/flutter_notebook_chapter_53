d58327b78012744fa32a867051c3df9797f155b6
JAICHANGPARK <aristojeff@gmail.com>
2026-09-13 11:04:38 +0900

    Update mental_sleap_segment.dart

--- lib/ep2674_mental_health_app/model/mental_sleap_segment.dart
+++ lib/ep2674_mental_health_app/model/mental_sleap_segment.dart
@@ -1,9 +1,10 @@
 import 'dart:ui';
 
-class SleepSegment{
+class SleepSegment {
   final String name;
   final String durationString;
   final int minutes;
   final Color color;
 
+  SleepSegment({this.name, this.durationString, this.minutes, this.color});
 }
