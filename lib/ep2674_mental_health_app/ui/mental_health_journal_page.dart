cc0569181704011f9445a53b83a57c62b0be7897
JAICHANGPARK <aristojeff@gmail.com>
2026-09-14 18:54:37 +0900

    Create mental_health_journal_page.dart

---
+++ lib/ep2674_mental_health_app/ui/mental_health_journal_page.dart
@@ -0,0 +1,16 @@
+import 'package:material_ui/material_ui.dart';
+
+
+class MentalHealthJournalPage extends StatefulWidget {
+  const MentalHealthJournalPage({super.key});
+
+  @override
+  State<MentalHealthJournalPage> createState() => _MentalHealthJournalPageState();
+}
+
+class _MentalHealthJournalPageState extends State<MentalHealthJournalPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
