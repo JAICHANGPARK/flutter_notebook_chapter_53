9068c00530053b45278bf7014165c201e99f0cce
Dreamwalker <aristojeff@gmail.com>
2026-09-15 18:08:44 +0900

    mental_barchart_painter.dart 업데이트

--- lib/ep2674_mental_health_app/painter/mental_barchart_painter.dart
+++ lib/ep2674_mental_health_app/painter/mental_barchart_painter.dart
@@ -12,6 +12,7 @@ class MentalBarchartPainter extends CustomPainter {
     final barWidth = (size.width - (spacing * (barCount - 1))) / barCount;
 
     final paint = Paint()..style = .fill;
+
     for (int i = 0; i < barCount; i++) {
       final x = i * (barWidth + spacing);
       final h = size.height * barHeights[i];
