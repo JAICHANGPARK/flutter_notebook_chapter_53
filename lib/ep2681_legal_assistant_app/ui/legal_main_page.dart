653c99cfc7a99eb0428d9d27b477e885741fbe08
JAICHANGPARK <aristojeff@gmail.com>
2026-09-17 18:19:03 +0900

    Create legal_main_page.dart

---
+++ lib/ep2681_legal_assistant_app/ui/legal_main_page.dart
@@ -0,0 +1,16 @@
+import 'package:material_ui/material_ui.dart';
+
+
+class LegalMainPage extends StatefulWidget {
+  const LegalMainPage({super.key});
+
+  @override
+  State<LegalMainPage> createState() => _LegalMainPageState();
+}
+
+class _LegalMainPageState extends State<LegalMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
