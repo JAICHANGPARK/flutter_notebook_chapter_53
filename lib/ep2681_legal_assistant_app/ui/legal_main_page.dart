76cd70e3cd37271018efbb537c5572ef088d4dd6
JAICHANGPARK <aristojeff@gmail.com>
2026-09-17 18:27:40 +0900

    Update legal_main_page.dart

--- lib/ep2681_legal_assistant_app/ui/legal_main_page.dart
+++ lib/ep2681_legal_assistant_app/ui/legal_main_page.dart
@@ -16,16 +16,6 @@ class _LegalMainPageState extends State<LegalMainPage> {
           Positioned.fill(
             child: Container(
               decoration: BoxDecoration(
-                // gradient: RadialGradient(
-                //   center: Alignment(-1.0, -0.8), // 좌측 상단 모서리에 광원 배치
-                //   radius: 1.6,                   // 화면 전체로 퍼지는 반경
-                //   colors: [
-                //     Color.fromRGBO(35, 33, 56, 1),
-                //     Colors.black,
-                //     Color.fromRGBO(66, 63, 64, 1),        // 외곽 완전 어두운 톤
-                //   ],
-                //   stops: [0.0, 0.5, 1.0],
-                // ),
                 gradient: LinearGradient(
                   begin: .topLeft,
                   end: .bottomLeft,
@@ -46,16 +36,6 @@ class _LegalMainPageState extends State<LegalMainPage> {
             child: Container(
               height: 240,
               decoration: BoxDecoration(
-                // gradient: RadialGradient(
-                //   center: Alignment(-1.0, -0.8), // 좌측 상단 모서리에 광원 배치
-                //   radius: 1.6,                   // 화면 전체로 퍼지는 반경
-                //   colors: [
-                //     Color.fromRGBO(35, 33, 56, 1),
-                //     Colors.black,
-                //     Color.fromRGBO(66, 63, 64, 1),        // 외곽 완전 어두운 톤
-                //   ],
-                //   stops: [0.0, 0.5, 1.0],
-                // ),
                 gradient: LinearGradient(
                   begin: .bottomLeft,
                   end: .topCenter,
