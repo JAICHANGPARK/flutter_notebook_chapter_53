8fe4c926062ca6d73bac693f9cf7e31b292e618e
JAICHANGPARK <aristojeff@gmail.com>
2026-09-19 08:35:12 +0900

    Update legal_home_page.dart

--- lib/ep2681_legal_assistant_app/ui/legal_home_page.dart
+++ lib/ep2681_legal_assistant_app/ui/legal_home_page.dart
@@ -23,8 +23,14 @@ class _LegalHomePageState extends State<LegalHomePage> {
                 child: Row(children: [CircleAvatar(), Text("Dream")]),
               ),
               Container(
-                decoration: BoxDecoration(shape: .circle),
-                child: Icon(Icons.star_border),
+                decoration: BoxDecoration(
+                  shape: .circle,
+                  color: Colors.black,
+                  boxShadow: [
+                    BoxShadow(color: Colors.green, offset: Offset(0, 1)),
+                  ],
+                ),
+                child: Icon(Icons.star_border, color: Colors.white,),
               ),
             ],
           ),
