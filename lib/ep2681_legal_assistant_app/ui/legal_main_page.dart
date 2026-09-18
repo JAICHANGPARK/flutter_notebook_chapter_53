6085c622e251db75568e5a02653a89c62d5b1b78
JAICHANGPARK <aristojeff@gmail.com>
2026-09-18 17:59:27 +0900

    Update legal_main_page.dart

--- lib/ep2681_legal_assistant_app/ui/legal_main_page.dart
+++ lib/ep2681_legal_assistant_app/ui/legal_main_page.dart
@@ -73,7 +73,8 @@ class _LegalMainPageState extends State<LegalMainPage> {
                   child: Row(
                     mainAxisAlignment: .spaceBetween,
                     children: [
-                      CircleAvatar(radius: 32, backgroundColor: Colors.white),
+                      CircleAvatar(radius: 32, backgroundColor: Colors.white,
+                      child: Icon(Icons.balance),),
                       CircleAvatar(
                         radius: 32,
                         backgroundColor: Color.fromRGBO(58, 58, 58, 1),
