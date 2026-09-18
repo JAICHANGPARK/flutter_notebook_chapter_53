7ad42db9bde1b56f4e27c966efe61b49f55828fb
JAICHANGPARK <aristojeff@gmail.com>
2026-09-19 08:36:41 +0900

    Update legal_home_page.dart

--- lib/ep2681_legal_assistant_app/ui/legal_home_page.dart
+++ lib/ep2681_legal_assistant_app/ui/legal_home_page.dart
@@ -13,30 +13,46 @@ class _LegalHomePageState extends State<LegalHomePage> {
     return SafeArea(
       child: Column(
         children: [
-          Row(
-            children: [
-              Container(
-                padding: .only(left: 4, right: 16, bottom: 4, top: 4),
-                decoration: ShapeDecoration(
-                  shape: StadiumBorder(),
-                  color: Colors.purple[50]!,
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              spacing: 14,
+              children: [
+                Container(
+                  padding: .only(left: 4, right: 16, bottom: 4, top: 4),
+                  decoration: ShapeDecoration(
+                    shape: StadiumBorder(),
+                    color: Colors.purple[50]!,
+                  ),
+                  child: Row(children: [CircleAvatar(), Text("Dream")]),
                 ),
-                child: Row(children: [CircleAvatar(), Text("Dream")]),
-              ),
-              Spacer(),
-              Container(
-                height: 48,
-                width: 48,
-                decoration: BoxDecoration(
-                  shape: .circle,
-                  color: Colors.black,
-                  boxShadow: [
-                    BoxShadow(color: Colors.green, offset: Offset(0, 1)),
-                  ],
+                Spacer(),
+                Container(
+                  height: 48,
+                  width: 48,
+                  decoration: BoxDecoration(
+                    shape: .circle,
+                    color: Colors.black,
+                    boxShadow: [
+                      BoxShadow(color: Colors.green, offset: Offset(0, 1)),
+                    ],
+                  ),
+                  child: Icon(Icons.star_border, color: Colors.white),
                 ),
-                child: Icon(Icons.star_border, color: Colors.white),
-              ),
-            ],
+                Container(
+                  height: 48,
+                  width: 48,
+                  decoration: BoxDecoration(
+                    shape: .circle,
+                    color: Colors.black,
+                    boxShadow: [
+                      BoxShadow(color: Colors.grey, offset: Offset(0, 1)),
+                    ],
+                  ),
+                  child: Icon(Icons.search, color: Colors.white),
+                ),
+              ],
+            ),
           ),
         ],
       ),
