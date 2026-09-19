929a03ce729fdf1bd4579cd40897219e8a92f728
JAICHANGPARK <aristojeff@gmail.com>
2026-09-20 08:45:39 +0900

    Update legal_home_page.dart

--- lib/ep2681_legal_assistant_app/ui/legal_home_page.dart
+++ lib/ep2681_legal_assistant_app/ui/legal_home_page.dart
@@ -169,8 +169,9 @@ class _LegalHomePageState extends State<LegalHomePage> {
 
                 final matrix = Matrix4.identity()
                   ..setEntry(3, 2, .001)
-                  ..rotateY(offset * -.2)
-                  ..rotateX(offset * .08)
+                  ..rotateY(offset * -.12)
+                  ..rotateZ(offset * .02)
+                  // ..rotateX(offset * .08)
                   ..scale(1.0 - (offset.abs() * .08).clamp(0, .2));
 
                 return Transform(
