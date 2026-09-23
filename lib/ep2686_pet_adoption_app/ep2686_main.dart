baf683656925e74a1536b50706faaba141e9ebda
JAICHANGPARK <aristojeff@gmail.com>
2026-09-23 18:35:35 +0900

    Update ep2686_main.dart

--- lib/ep2686_pet_adoption_app/ep2686_main.dart
+++ lib/ep2686_pet_adoption_app/ep2686_main.dart
@@ -1,5 +1,6 @@
+import 'package:flutter_notebook_chapter_53/ep2686_pet_adoption_app/pet_adoption_app.dart';
 import 'package:material_ui/material_ui.dart';
 
 void main() {
-
+  runApp(PetAdoptionApp());
 }
