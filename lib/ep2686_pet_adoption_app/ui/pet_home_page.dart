44190a7c67b97d48f779e9e500f2f680b44c5801
JAICHANGPARK <aristojeff@gmail.com>
2026-09-23 18:34:01 +0900

    Update pet_home_page.dart

---
+++ lib/ep2686_pet_adoption_app/ui/pet_home_page.dart
@@ -0,0 +1,15 @@
+import 'package:material_ui/material_ui.dart';
+
+class PetHomePage extends StatefulWidget {
+  const PetHomePage({super.key});
+
+  @override
+  State<PetHomePage> createState() => _PetHomePageState();
+}
+
+class _PetHomePageState extends State<PetHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
