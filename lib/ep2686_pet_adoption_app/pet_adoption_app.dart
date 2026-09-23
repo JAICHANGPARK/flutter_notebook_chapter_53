134c6ccd2230956a5684f542c80037c82ed8f87d
JAICHANGPARK <aristojeff@gmail.com>
2026-09-23 18:34:34 +0900

    Update pet_adoption_app.dart

--- lib/ep2686_pet_adoption_app/pet_adoption_app.dart
+++ lib/ep2686_pet_adoption_app/pet_adoption_app.dart
@@ -1,11 +1,15 @@
 import 'package:material_ui/material_ui.dart';
 
+import 'ui/pet_main_page.dart';
+
 
 class PetAdoptionApp extends StatelessWidget {
   const PetAdoptionApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: PetMainPage(),
+    );
   }
 }
