073eff3362eaaefb5c03e4f9bf102ea901968944
JAICHANGPARK <aristojeff@gmail.com>
2026-09-26 10:37:38 +0900

    Update pet_home_page.dart

--- lib/ep2686_pet_adoption_app/ui/pet_home_page.dart
+++ lib/ep2686_pet_adoption_app/ui/pet_home_page.dart
@@ -14,39 +14,42 @@ class _PetHomePageState extends State<PetHomePage> {
       crossAxisAlignment: .start,
 
       children: [
-        Row(
-          spacing: 16,
-          children: [
-            Expanded(
-              child: Column(
-                crossAxisAlignment: .start,
-                children: [
-                  Text("Hello, Dream"),
-                  Text("Good pets make a great life!"),
-                ],
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Row(
+            spacing: 12,
+            children: [
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: .start,
+                  children: [
+                    Text("Hello, Dream"),
+                    Text("Good pets make a great life!"),
+                  ],
+                ),
               ),
-            ),
-            Container(
-              padding: .all(8),
-              decoration: BoxDecoration(
-                  shape: .circle,
-                  color: Colors.white,
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withValues(alpha: .1),
-                      spreadRadius: 1,
-                      blurRadius: 3
-                    )
-                  ]
+              Container(
+                padding: .all(8),
+                decoration: BoxDecoration(
+                    shape: .circle,
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withValues(alpha: .1),
+                        spreadRadius: 1,
+                        blurRadius: 3
+                      )
+                    ]
+                ),
+                child: Badge(
+                  child: Icon(Icons.notifications_none),
+                ),
               ),
-              child: Badge(
-                child: Icon(Icons.notifications_none),
-              ),
-            ),
-            CircleAvatar(
-              radius: 24,
-            )
-          ],
+              CircleAvatar(
+                radius: 24,
+              )
+            ],
+          ),
         ),
 
       ],
